c6ea7510f94832fb127e5160c45636cf7596d636
Michal Hoftich <michal.h21@gmail.com>
2021-07-29 16:19:02 +0200

    try to print the make4ht-logging file

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ export TEXINPUTS=.:/root/texmf//:
 # debugging messages
 kpsewhich make4ht-logging.lua
 which make4ht
+cat /root/texmf/scripts/lua/make4ht/make4ht-logging.lua
 echo $command
 eval $command
 
