b7a6b2f5ae8c57e244fc402d5ba82022ccfe1568
Michal Hoftich <michal.h21@gmail.com>
2021-07-29 16:49:35 +0200

    maybe this will work

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,8 @@ then
   apt-get install $extra_packages
 fi
 
-export TEXINPUTS=.:/root/texmf//:
 # debugging messages
-kpsewhich make4ht-logging.lua
-which make4ht
-cat /root/texmf/scripts/lua/make4ht/make4ht-logging.lua
 echo $command
-eval $command
+( TEXINPUTS=.:/root/texmf//: $command )
 
 
