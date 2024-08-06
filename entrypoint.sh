beddab9e662f71793623868a44ae9f29af380e6c
Michal Hoftich <michal.h21@gmail.com>
2024-08-06 22:28:51 +0200

    run apt get update before installing extra packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -z $extra_packages ]  
 then
-  apt-get install -y $extra_packages
+  apt-get update && apt-get install -y $extra_packages
 fi
 
 export TEXINPUTS=.:/root/texmf//:
